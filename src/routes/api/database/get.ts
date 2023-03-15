f2d0ffbed9fab618666dba1c2f3b52ed56ed1922
StefanoVe <stefano.vecchietti.99@gmail.com>
2023-03-15 18:00:30 +0100

    route

--- src/routes/api/database/get.ts
+++ src/routes/api/database/get.ts
@@ -1,6 +1,8 @@
 //boiler plate for an express post request
 import express from 'express';
 import { param } from 'express-validator';
+import { Types } from 'mongoose';
+import { DatabaseModel } from '../../../models/database.js';
 import { validateRequest } from '../../../services/validation/service.validate-request.js';
 
 const router = express.Router();
@@ -9,10 +11,23 @@ router.get(
   '/:id?',
   param('id').optional().isMongoId().withMessage('id must be a valid mongo id'),
   validateRequest,
-  (req, res) => {
+  async (req, res) => {
     const { id } = req.params;
 
-    res.send(`db id: ${id}`);
+    if (!id?.length) {
+      const database = await DatabaseModel.find({});
+
+      return res.send(database);
+    }
+
+    const _mongoId = new Types.ObjectId(id);
+    const databases = await DatabaseModel.findById(_mongoId);
+
+    if (!databases) {
+      return res.status(404).send('database not found');
+    }
+
+    res.send(databases);
   }
 );
 
