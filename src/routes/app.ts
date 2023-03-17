86abdb5cc4bc7ce99332803b57a73952528168a9
StefanoVe <stefano.vecchietti.99@gmail.com>
2023-03-17 13:30:04 +0100

    closes #1

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -2,14 +2,12 @@ import cors from 'cors';
 import express from 'express';
 import { NotFoundError } from '../errors/errors/not-found-error.js';
 import { errorHandler } from '../errors/middlewares/index.js';
-import { requireApiKey } from '../services/validation/service.require-api-key.js';
 import { apiRouter } from './api/index.js';
 
 export const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(requireApiKey);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
