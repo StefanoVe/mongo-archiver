e261e07e00436e0af37189dfe76a48189e748794
StefanoVe <stefano.vecchietti.99@gmail.com>
2023-03-21 15:16:34 +0100

    route logging

--- src/services/validation/service.require-api-key.ts
+++ src/services/validation/service.require-api-key.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import { declareEnvs } from '../service.utils.js';
+import { colorfulLog, declareEnvs } from '../service.utils.js';
 
 const { API_KEY } = declareEnvs(['API_KEY']);
 
@@ -8,11 +8,18 @@ export const requireApiKey = (
   res: Response,
   next: NextFunction
 ) => {
+  const route = `${req.method} ${req.path}`;
+  const timestamp = new Date().toISOString();
+
   if (req.headers['x-api-key'] !== API_KEY) {
+    colorfulLog(`${timestamp} | ${route} | Invalid API Key `, 'error');
     res.status(401).send({
       error: 'Not authorized',
     });
+    return;
   }
+
+  colorfulLog(`${timestamp} | ${route}`, 'info');
 
   next();
 };
