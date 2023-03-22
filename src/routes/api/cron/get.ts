6b4ad371e6892b1d68720855798daea7887dc163
StefanoVe <stefano.vecchietti.99@gmail.com>
2023-03-22 14:02:44 +0100

    get route cronjobs

--- src/routes/api/cron/get.ts
+++ src/routes/api/cron/get.ts
@@ -2,32 +2,38 @@
 import express from 'express';
 import { param } from 'express-validator';
 import { Types } from 'mongoose';
-import { DatabaseModel } from '../../../models/database.js';
+import {
+  CronJobModel,
+  CRON_JOB_VALIDATION_MESSAGES,
+} from '../../../models/cron-job.js';
 import { validateRequest } from '../../../services/validation/service.validate-request.js';
 
 const router = express.Router();
 
 router.get(
   '/:id?',
-  param('id').optional().isMongoId().withMessage('id must be a valid mongo id'),
+  param('id')
+    .optional()
+    .isMongoId()
+    .withMessage(CRON_JOB_VALIDATION_MESSAGES._id),
   validateRequest,
   async (req, res) => {
     const { id } = req.params;
 
     if (!id?.length) {
-      const database = await DatabaseModel.find({});
+      const database = await CronJobModel.find({}).populate('databases');
 
       return res.send(database);
     }
 
     const _mongoId = new Types.ObjectId(id);
-    const databases = await DatabaseModel.findById(_mongoId);
+    const cronjob = await CronJobModel.findById(_mongoId).populate('databases');
 
-    if (!databases) {
-      return res.status(404).send({ error: 'database not found' });
+    if (!cronjob) {
+      return res.status(404).send({ error: 'cronjob not found' });
     }
 
-    res.send(databases);
+    res.send(cronjob);
   }
 );
 
