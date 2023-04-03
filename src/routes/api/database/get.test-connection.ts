b2f903841bd491e65e6b6b06948ff95e1e362e72
StefanoVe <stefano.vecchietti.99@gmail.com>
2023-04-03 17:38:52 +0200

    removes validation

--- src/routes/api/database/get.test-connection.ts
+++ src/routes/api/database/get.test-connection.ts
@@ -5,34 +5,27 @@ import { DATABASE_VALIDATION_MESSAGES } from '../../../models/database.js';
 import { testConnection } from '../../../services/connect.js';
 import { validateRequest } from '../../../services/validation/service.validate-request.js';
 
-const _validateMongoUri = (uri: string) => {
-  //validate the uri with a regex and return true if it's valid
-  const url =
-    /^mongodb:\/\/[a-zA-Z0-9]+:[a-zA-Z0-9]+@([a-zA-Z0-9]+\.){1,}[a-zA-Z0-9]+(:[0-9]+)?\/[a-zA-Z0-9]+$/;
+const _validateMongoUri = (value: string) => {
+  // MongoDB URL regex pattern
+  const pattern =
+    /^mongodb(?:\+srv)?:\/\/(?:(?:(?:[\w-]+):(?:[\w-]+)@)?(?:[\w-]+\.)+[\w-]+(?::\d{1,5})?)(?:\/(?:[\w-]+(?:,[\w-]+)*)?(?:\?[\w-]+(?:=[\w-]+(?:&[\w-]+(?:=[\w-]+)*)*)?)?)?$/;
 
-  const ipAddress =
-    /^([01]?\d?\d|2[0-4]\d|25[0-5])(\.([01]?\d?\d|2[0-4]\d|25[0-5])){3}$/;
-
-  //localhost with port
-  const localhost = /^mongodb:\/\/localhost:\d+\/\w+/;
-
-  return uri.match(url) || uri.match(ipAddress) || uri.match(localhost)
-    ? true
-    : false;
+  if (!pattern.test(value)) {
+    return false;
+  }
+  return true;
 };
 
 const router = express.Router();
 
 router.post(
   '/',
-  body('uri')
-    .custom(_validateMongoUri)
-    .withMessage(DATABASE_VALIDATION_MESSAGES.uri),
+  body('url').isString().withMessage(DATABASE_VALIDATION_MESSAGES.uri),
   validateRequest,
   async (req, res) => {
-    const { uri } = req.body;
+    const { url } = req.body;
 
-    const result = await testConnection(uri);
+    const result = await testConnection(url);
 
     if (!result) {
       return res.status(500).send({ error: 'connection failed' });
